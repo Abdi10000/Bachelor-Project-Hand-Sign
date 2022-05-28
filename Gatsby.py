554626b7bc34ba8ed5a2db32f510a29f326d9e06
Kristina <72916303+Ina552f@users.noreply.github.com>
2022-05-28 14:50:41 +0200

    Update Gatsby.py
    Rengørelse

--- Gatsby.py
+++ Gatsby.py
@@ -3,7 +3,6 @@
 # Dato 01/06-2022
 # Koden oversætter hånd tegn til lyd
 
-
 # Dependencies for Gatsby prototypen
 import cv2 as cv
 import mediapipe as mp
@@ -11,15 +10,13 @@ import os
 import pyttsx3
 import threading
 
-
-# variablen for outputting lyd
+# Variable for outputting lyd
 engine = pyttsx3.init()
 
-
-# en klasse der bruges til at opfange hånden ved brug af mediapipe
+# En klasse der bruges til at opfange hånden ved brug af mediapipe
 class handTracker():
 
-    # en funktion der bruges til at opfange hånden ved brug af mediapipe
+    # En funktion der opfanger hånden ved brug af mediapipe
     def __init__(self, mode=False, maxHands=1, detectionCon=0.5, modelComplexity=1, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
@@ -31,7 +28,7 @@ class handTracker():
         self.mpDraw = mp.solutions.drawing_utils
 
 
-    # funktion der tegner og finder hånden
+    # Funktion der tegner og finder hånd
     def handsFinder(self, image, draw=True):
         imageRGB = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         self.results = self.hands.process(imageRGB)
@@ -43,7 +40,7 @@ class handTracker():
         return image
 
 
-    # funktion der beregner og viser positioner på hånden
+    # Funktion der beregner og viser positioner på hånden
     def positionFinder(self, image, handNo=0, draw=True):
         lmlist = []
         if self.results.multi_hand_landmarks:
@@ -58,7 +55,7 @@ class handTracker():
         return lmlist
 
 
-# højtaler funktionen til at omdanne tekst til lyd
+# Højtaler funktion til at omdanne tekst til lyd
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -76,22 +73,21 @@ def main():
     webcamWidth = 1200
     webcamHeight = 800
 
-    #
     webcam = cv.VideoCapture(0)
 
-    # Adjusting the webcam width and height
+    # Justere kamera bredde og højde
     webcam.set(cv.CAP_PROP_FRAME_WIDTH, webcamWidth)
     webcam.set(cv.CAP_PROP_FRAME_HEIGHT, webcamHeight)
 
 
-    # variablen for at anvende handTracker class
+    # Variablen for at anvende handTracker class
     tracker = handTracker()
     folderPath = "Pictures"
     myList = os.listdir(folderPath)
     #print(myList)
 
 
-    # denne array indeholder listen af hånd tegn billeder
+    # Denne array indeholder listen af håndtegn billeder
     overlayList = []
     for imagePath in myList:
         image = cv.imread(f'{folderPath}/{imagePath}')
@@ -100,35 +96,27 @@ def main():
         detector = handTracker(detectionCon=0.75)
 
 
-
-    # A while loop which/that is handling webcam screen continuation
+    # Et while loop der håndtere webkam skærm forsættelse
     while True:
 
-        #
         success, image = webcam.read()
 
-        #
         image = tracker.handsFinder(image)
 
-        # flips the camera and makes sure that the webcam is not inverted(spejlvendt)
+        # Vender kameraet og sørger for at webkameraet ikke er omvendt (spejlvendt)
         image = cv.flip(image, 1)
 
-        #
         lmList = tracker.positionFinder(image)
 
-
-        #
         if len(lmList) != 0:
 
-            #
             if lmList[8][2] > lmList[6][2] and lmList[12][2] > lmList[10][2] and lmList[16][2] > lmList[14][2] and lmList[20][2] > lmList[18][2]:
                 image[0:120, 0:120] = overlayList[0]
                 speech = "Hello"
                 cv.putText(image, speech, (0, 400), cv.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 4, cv.LINE_AA)
                 fasterCode(speak, speech)
 
-
-            #
+                
             if lmList[8][2] < lmList[6][2] and lmList[12][2] > lmList[10][2] and lmList[16][2] > lmList[14][2] and lmList[20][2] > lmList[18][2]:
                 image[0:120, 0:120] = overlayList[1]
                 speech = "Goodbye"
@@ -136,7 +124,6 @@ def main():
                 fasterCode(speak, speech)
 
 
-            #
             if lmList[8][2] < lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] > lmList[14][2] and lmList[20][2] > lmList[18][2]:
                 image[0:120, 0:120] = overlayList[2]
                 speech = "Thank you"
@@ -144,7 +131,6 @@ def main():
                 fasterCode(speak, speech)
 
 
-            #
             if lmList[8][2] < lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] < lmList[14][2] and lmList[20][2] > lmList[18][2]:
                 image[0:120, 0:120] = overlayList[3]
                 speech = "Can you help me"
@@ -152,7 +138,6 @@ def main():
                 fasterCode(speak, speech)
 
 
-            #
             if lmList[8][2] < lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] < lmList[14][2] and lmList[20][2] < lmList[18][2]:
                 image[0:120, 0:120] = overlayList[4]
                 speech = "I want to buy"
@@ -160,15 +145,12 @@ def main():
                 fasterCode(speak, speech)
 
 
-            #
             if lmList[8][2] < lmList[6][2] and lmList[12][2] > lmList[10][2] and lmList[16][2] > lmList[14][2] and lmList[20][2] < lmList[18][2]:
                 image[0:120, 0:120] = overlayList[5]
                 speech = "How do i find"
                 cv.putText(image, speech, (0, 400), cv.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 4, cv.LINE_AA)
                 fasterCode(speak, speech)
 
-
-            #
             if lmList[8][2] > lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] < lmList[14][2] and lmList[20][2] > lmList[18][2]:
                 image[0:120, 0:120] = overlayList[6]
                 speech = "I am deaf"
@@ -176,7 +158,6 @@ def main():
                 fasterCode(speak, speech)
 
 
-            #
             if lmList[8][2] > lmList[6][2] and lmList[12][2] > lmList[10][2] and lmList[16][2] > lmList[14][2] and lmList[20][2] < lmList[18][2]:
                 image[0:120, 0:120] = overlayList[7]
                 speech = "Can you repeat"
@@ -184,15 +165,13 @@ def main():
                 fasterCode(speak, speech)
 
 
-            #
             if lmList[8][2] < lmList[6][2] and lmList[12][2] > lmList[10][2] and lmList[16][2] < lmList[14][2] and lmList[20][2] < lmList[18][2]:
                 image[0:120, 0:120] = overlayList[8]
                 speech = "okay"
                 cv.putText(image, speech, (0, 400), cv.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 4, cv.LINE_AA)
                 fasterCode(speak, speech)
 
-
-            #
+                
             if lmList[8][2] > lmList[6][2] and lmList[12][2] < lmList[10][2] and lmList[16][2] < lmList[14][2] and lmList[20][2] < lmList[18][2]:
                 image[0:120, 0:120] = overlayList[9]
                 speech = "stop"
